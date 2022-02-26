b623371cee56995d0c61c18c8de564245b906206
ironcodemaniac <96331288+ironcodemaniac@users.noreply.github.com>
2022-02-26 16:23:15 +0700

    add the radio button feature

--- app.js
+++ app.js
@@ -5,23 +5,40 @@ for (let i=33; i<127; i++){
   arrayChar.push(String.fromCharCode(i));
 }
 
-function getRandomIndex(){
-    let randomNumber = Math.floor(Math.random()*arrayChar.length);
-    return randomNumber
+// Set global variable
+let resultONe_El = document.getElementById("result-box-one");
+let resultTwo_El = document.getElementById("result-box-two");
+let resultThree_El = document.getElementById("result-box-three");
+let resultFour_El = document.getElementById("result-box-four");
+
+
+function getPasswordLength(){
+  let length = 0;
+  if (document.getElementById("passwordlength-5-El").checked){
+    length = document.getElementById("passwordlength-5-El").value;
+  } 
+  else if (document.getElementById("passwordlength-10-El").checked){
+    length = document.getElementById("passwordlength-10-El").value;
+  } 
+  else {
+    length = document.getElementById("passwordlength-15-El").value;
+  }
+  return length;
 }
 
 function getPasswords(){
-    let randomPassword = arrayChar[getRandomIndex()];
-    for (let i=0; i<14; i++){
-    randomPassword += arrayChar[getRandomIndex()];
+    let randomPassword = "";
+    for (let i=0; i<getPasswordLength(); i++){
+    let randomNumber = Math.floor(Math.random()*arrayChar.length); //should declare the variable here, so the randomNumber can be generated every time.
+    randomPassword += arrayChar[randomNumber];
     }
     return randomPassword;
 }
 
 function generatePasswords(){
-    document.getElementById("result-box-one").textContent = getPasswords();
-    document.getElementById("result-box-two").textContent = getPasswords();
-    document.getElementById("result-box-three").textContent = getPasswords();
-    document.getElementById("result-box-four").textContent = getPasswords();
+    resultONe_El.textContent = getPasswords();
+    resultTwo_El.textContent = getPasswords();
+    resultThree_El.textContent = getPasswords();
+    resultFour_El.textContent = getPasswords();
 }
 
